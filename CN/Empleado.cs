450d866ba0a0bd1fe109ab09baf4a1d8c5cd1d43
cetiuanl <erolando.avila@aol.com>
2018-04-28 13:12:58 -0600

    primer commit

---
+++ CN/Empleado.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CN
+{
+    public class Empleado : Persona
+    {
+        public string usuario { get; set; }
+        public int idEmpleado { get; set; }
+
+        public Empleado(string _usuario,
+            int _idEmpleado,
+            string _nombreCompleto, 
+            string _direccion, 
+            DateTime _fechaNac)
+            : base(_nombreCompleto, _direccion, _fechaNac)
+        {
+            this.usuario = _usuario;
+            this.idEmpleado = _idEmpleado;
+        }
+
+        public override string descripcion()
+        {
+            string result = String.Format("usuario:{0} - idEmpleado: {1} - {2}",
+                usuario, idEmpleado, base.descripcion());
+
+            return result;
+        }
+    }
+}
---
+++ CN/Persona.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CN
+{
+    public class Persona
+    {
+        #region Propiedades
+
+        private string _nombreCompleto;
+
+        public string nombreCompleto
+        {
+            get { return _nombreCompleto; }
+            //set { _nombreCompleto = value; }
+        }
+
+        public string direccion { get; set; }
+        
+        private DateTime _fechaNacimiento;
+
+        public DateTime fechaNacimiento
+        {
+            get { return _fechaNacimiento; }
+            //set { _fechaNacimiento = value; }
+        }
+        
+        public int edad
+        {
+            get {
+                DateTime hoy = DateTime.Today;
+                int edad = hoy.Year - fechaNacimiento.Year;
+
+                if (hoy < fechaNacimiento.AddYears(edad))
+                {
+                    edad--;
+                }
+
+                return edad;
+            }
+        }        
+
+        #endregion
+
+        #region Constructores
+
+        public Persona(string _nombreCompleto,
+                        string _direccion,
+                        DateTime _fechaNacimiento)
+        {
+            this._nombreCompleto = _nombreCompleto;
+            this.direccion = _direccion;
+            this._fechaNacimiento = _fechaNacimiento;
+        }
+
+        #endregion
+
+        #region Metodos y Funciones
+        public virtual string descripcion()
+        {
+            string resultado = string.Format("Nombre: {0} - Direccion: {1} - Fecha Nacimiento: {2} - Edad: {3}",
+                nombreCompleto, direccion, fechaNacimiento, edad);
+
+            return resultado;
+        }
+
+        #endregion        
+    }
+}
